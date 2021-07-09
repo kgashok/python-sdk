cb3726ba4e2990f8bba17f7b5a3636e85a4e239e
Ashok Bakthavathsalam <kgashok@users.noreply.github.com>
2021-07-09 23:57:19 +0530

    success in classifying an URL

--- examples/natural_language_understanding_v1.py
+++ examples/natural_language_understanding_v1.py
@@ -1,7 +1,7 @@
 import json
 from ibm_watson import NaturalLanguageUnderstandingV1
-from ibm_watson.natural_language_understanding_v1 import Features, EntitiesOptions, KeywordsOptions
-# from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson.natural_language_understanding_v1 import Features, EntitiesOptions, KeywordsOptions, CategoriesOptions
+#from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 # Authentication via IAM
 # authenticator = IAMAuthenticator('your_api_key')
@@ -13,12 +13,23 @@ from ibm_watson.natural_language_understanding_v1 import Features, EntitiesOptio
 # Authentication via external config like VCAP_SERVICES
 service = NaturalLanguageUnderstandingV1(
     version='2018-03-16')
-service.set_service_url('https://gateway.watsonplatform.net/natural-language-understanding/api')
+#service.set_service_url('https://gateway.watsonplatform.net/natural-language-understanding/api')
 
+#response = service.analyze(
+#    text='Bruce Banner is the Hulk and Bruce Wayne is BATMAN! '
+#    'Superman fears not Banner, but Wayne.',
+#    features=Features(entities=EntitiesOptions(),
+#                      keywords=KeywordsOptions())).get_result()
+
+#print(json.dumps(response, indent=2))
+
+nlu_url = "http://newsroom.ibm.com/Guerbet-and-IBM-Watson-Health-Announce-Strategic-Partnership-for-Artificial-Intelligence-in-Medical-Imaging-Liver"
 response = service.analyze(
-    text='Bruce Banner is the Hulk and Bruce Wayne is BATMAN! '
-    'Superman fears not Banner, but Wayne.',
-    features=Features(entities=EntitiesOptions(),
-                      keywords=KeywordsOptions())).get_result()
+    url=nlu_url,
+    features=Features(categories=CategoriesOptions(), \
+                      #entities=EntitiesOptions(),
+                      #keywords=KeywordsOptions())
+                      )
+).get_result()
 
 print(json.dumps(response, indent=2))
